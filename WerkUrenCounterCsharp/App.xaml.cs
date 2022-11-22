94832ac88f3ba1d74433013242bf4e49ce06d166
Xander Aerts <xanderaerts@gmail.com>
2022-11-22 12:00:52 +0100

    start

---
+++ WerkUrenCounterCsharp/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WerkUrenCounterCsharp;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ WerkUrenCounterCsharp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WerkUrenCounterCsharp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
